57ac5ce77749699b6f9cb42f65d0d318393f05f1
Malsha Jayakody <80629089+Malsha2000@users.noreply.github.com>
2022-11-02 10:25:58 +0530

    make routes

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Login from "../Components/LoginForm";
+import Register from '../Pages/Register'
+import InspectionAdd from "../Pages/inspectionAdd";
+import InspectionStart from "../Pages/InspectionStart";
+import InspectionList from "../Pages/Inspection/InspectionList";
+import InspectionTable from "../Pages/Inspection/InspectionTable";
+
 
 const PageRoutes = () => {
 	return (
@@ -9,6 +15,11 @@ const PageRoutes = () => {
 			<BrowserRouter>
 				<Routes>
 					<Route path="/" element={<Login />}></Route>
+					<Route path='/user/add' element={<Register/>}></Route>
+					<Route path='/inspection/add' element={<InspectionAdd/>}></Route>
+					<Route path='/inspection/card' element={<InspectionStart/>}></Route>
+					<Route path="/inspections" element={<InspectionList />} />
+					<Route path="/inspection/all" element={<InspectionTable />} />
 				</Routes>
 			</BrowserRouter>
 		</div>

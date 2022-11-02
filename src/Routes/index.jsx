2bdfe28c61c4916175d1cc5f7faf89fb0ad955dc
Malsha Jayakody <80629089+Malsha2000@users.noreply.github.com>
2022-11-02 18:30:31 +0530

    make routes

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -2,13 +2,12 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Login from "../Components/LoginForm";
-import Register from '../Pages/Register'
+import Register from "../Pages/Register";
 import InspectionAdd from "../Pages/inspectionAdd";
 import InspectionStart from "../Pages/InspectionStart";
 import InspectionList from "../Pages/Inspection/InspectionList";
 import InspectionTable from "../Pages/Inspection/InspectionTable";
 import UpdateInspection from "../Pages/Inspection/UpdateInsception";
-
 
 const PageRoutes = () => {
 	return (
@@ -16,15 +15,25 @@ const PageRoutes = () => {
 			<BrowserRouter>
 				<Routes>
 					<Route path="/" element={<Login />}></Route>
-					<Route path='/user/add' element={<Register/>}></Route>
-					<Route path='/inspection/add' element={<InspectionAdd/>}></Route>
-					<Route path='/inspection/card' element={<InspectionStart/>}></Route>
-					<Route path="/inspections" element={<InspectionList />} />
-					<Route path="/inspection/all" element={<InspectionTable />} />
+					<Route path="/user/add" element={<Register />}></Route>
 					<Route
-                            path="/inspection/update/:id"
-                            element={<UpdateInspection />}
-                        />
+						path="/inspection/add"
+						element={<InspectionAdd />}></Route>
+					<Route
+						path="/inspection/card"
+						element={<InspectionStart />}></Route>
+					<Route
+						path="/inspections"
+						element={<InspectionList />}
+					/>
+					<Route
+						path="/inspection/all"
+						element={<InspectionTable />}
+					/>
+					<Route
+						path="/inspection/update/:id"
+						element={<UpdateInspection />}
+					/>
 				</Routes>
 			</BrowserRouter>
 		</div>

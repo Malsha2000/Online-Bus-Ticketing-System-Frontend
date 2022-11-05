2bd5aaa4844aa679f08fd67b25746eaf6de73df0
Heshani Sandanika <74381597+heshani9918@users.noreply.github.com>
2022-11-05 21:12:56 +0530

    Implement routes

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -8,7 +8,9 @@ import InspectionStart from "../Pages/InspectionStart";
 import InspectionList from "../Pages/Inspection/InspectionList";
 import InspectionTable from "../Pages/Inspection/InspectionTable";
 import UpdateInspection from "../Pages/Inspection/UpdateInsception";
-
+import TimetableAdd from "../Pages/TimetableAdd";
+import TimetableTable from "../Pages/Timetable/TimetableTable";
+import UpdateTimetable from "../Pages/Timetable/UpdateTimetable";
 
 const PageRoutes = () => {
 	return (
@@ -25,6 +27,9 @@ const PageRoutes = () => {
                             path="/inspection/update/:id"
                             element={<UpdateInspection />}
                         />
+						<Route path='/busroutes/add' element={<TimetableAdd/>}></Route>
+						<Route path="/busroutes/all" element={<TimetableTable />} />
+						<Route path="/busroutes/update/:id" element={<UpdateTimetable />} />
 				</Routes>
 			</BrowserRouter>
 		</div>
